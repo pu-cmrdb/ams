06550fb227f15f091fe5c42a7e2650d5664b9f9a
Kamiya <kamiya@kamiya.app>
2026-05-02 17:16:14 +0800

    feat: selection display

--- src/app/(dashboard)/inventories/create/_components/asset-picker.tsx
+++ src/app/(dashboard)/inventories/create/_components/asset-picker.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import { SearchXIcon } from 'lucide-react';
+import { PlusIcon, SearchXIcon, Trash2Icon } from 'lucide-react';
+import { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
+import Image from 'next/image';
+
+import { Item, ItemActions, ItemContent, ItemDescription, ItemGroup, ItemMedia, ItemTitle } from '@/components/ui/item';
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
 import { Empty, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useTRPC } from '@/trpc/react';
 
 type AssetPickerProps = Readonly<{
@@ -64,6 +69,10 @@ export function AssetPicker({ onChange, value }: AssetPickerProps) {
     onChange(newValue);
   };
 
+  const onClear = () => {
+    onChange([]);
+  };
+
   const content = data?.map((category) => (
     <CommandGroup
       heading={
@@ -85,32 +94,96 @@ export function AssetPicker({ onChange, value }: AssetPickerProps) {
     </CommandGroup>
   ));
 
+  const assets = useMemo(
+    () => data?.flatMap((category) => category.assets),
+    [data],
+  );
+
   return (
-    <div>
-      <Popover>
-        <PopoverTrigger render={<Button>新增財產</Button>} />
-        <PopoverContent align="start" className="w-auto p-0">
-          <Command>
-            <CommandInput />
-            <CommandList>
-              <CommandEmpty>
-                <Empty>
-                  <EmptyMedia variant="icon">
-                    <SearchXIcon />
-                  </EmptyMedia>
-                  <EmptyHeader>
-                    <EmptyTitle>找不到相符的財產</EmptyTitle>
-                    <EmptyDescription>
-                      請確認拼字是否正確，或使用其他關鍵字搜尋
-                    </EmptyDescription>
-                  </EmptyHeader>
-                </Empty>
-              </CommandEmpty>
-              {content}
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-4">
+        <Popover>
+          <PopoverTrigger
+            render={
+              <Button>
+                <PlusIcon data-icon="inline-start" />
+                <span>新增財產</span>
+              </Button>
+            }
+          />
+          <PopoverContent align="start" className="w-auto p-0">
+            <Command>
+              <CommandInput />
+              <CommandList>
+                <CommandEmpty>
+                  <Empty>
+                    <EmptyMedia variant="icon">
+                      <SearchXIcon />
+                    </EmptyMedia>
+                    <EmptyHeader>
+                      <EmptyTitle>找不到相符的財產</EmptyTitle>
+                      <EmptyDescription>
+                        請確認拼字是否正確，或使用其他關鍵字搜尋
+                      </EmptyDescription>
+                    </EmptyHeader>
+                  </Empty>
+                </CommandEmpty>
+                {content}
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+        <Button onClick={onClear} variant="outline">
+          清除
+        </Button>
+      </div>
+      <ItemGroup>
+        {value.map((id) => {
+          const item = assets?.find((asset) => asset.id === id);
+
+          if (!item) {
+            return (
+              <Item key={id} variant="outline">
+                <ItemContent>
+                  <ItemTitle>
+                    <Skeleton className="w-12" />
+                  </ItemTitle>
+                  <ItemDescription>
+                    <Skeleton className="w-28" />
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+            );
+          }
+
+          return (
+            <Item key={id} variant="outline">
+              {item.imageHash && (
+                <ItemMedia variant="image">
+                  <Image
+                    alt={item.name}
+                    fill
+                    src={`/api/image/${item.imageHash}`}
+                  />
+                </ItemMedia>
+              )}
+              <ItemContent>
+                <ItemTitle>{item.name}</ItemTitle>
+                <ItemDescription>{item.description}</ItemDescription>
+              </ItemContent>
+              <ItemActions>
+                <Button
+                  onClick={() => onItemSelect(id)}
+                  size="icon"
+                  variant="destructive"
+                >
+                  <Trash2Icon />
+                </Button>
+              </ItemActions>
+            </Item>
+          );
+        })}
+      </ItemGroup>
     </div>
   );
 }
