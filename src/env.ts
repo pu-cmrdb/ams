5b02ff0ade4b975c6edc7e028fe08fc93435bf16
Kamiya <kamiya@kamiya.app>
2026-05-05 14:05:25 +0800

    chore: extract common components

--- src/env.ts
+++ src/env.ts
@@ -3,9 +3,6 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { type } from '@/lib/arktype';
 
 export const env = createEnv({
-  client: {
-    NEXT_PUBLIC_BETTER_AUTH_IAM_URL: type('string.url > 0'),
-  },
   emptyStringAsUndefined: true,
   runtimeEnv: {
     APP_URL:
@@ -22,8 +19,6 @@ export const env = createEnv({
         ? process.env.APP_PROD_URL
         : process.env.APP_DEV_URL,
     DATABASE_URL: process.env.DATABASE_URL,
-    NEXT_PUBLIC_BETTER_AUTH_IAM_URL:
-      process.env.NEXT_PUBLIC_BETTER_AUTH_IAM_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
   server: {
